d0601e4efa57cb5512427e27daacc2c5b4a70223
Michelle Darlea <mdarlea@gmail.com>
2015-07-15 22:38:45 -0400

    Added the sw.ui.bootstrap module

---
+++ gruntfile.js
@@ -0,0 +1,52 @@
+﻿module.exports = function (grunt) {
+    grunt.initConfig({
+        dist: 'dist',
+        pkg: grunt.file.readJSON('package.json'),
+        debug: {
+            options: {
+                open: false // do not open node-inspector in Chrome automatically 
+            }
+        },
+        build: {
+            options: {
+                filename: 'sw-ui-bootstrap',
+                modulePrefix: 'sw.ui.bootstrap.',
+                moduleName: 'sw.ui.bootstrap'
+            }
+        },
+        ngdocs: {
+            options: {
+                dest: '<%= dist %>/docs',
+                scripts: [
+                    'angular.js',
+                    '<%= concat.dist_tpls.dest %>'
+                ],
+                title: '<%= pkg.name %>',
+                html5Mode: false,
+                sourceLink: 'https://github.com/mdarlea/sw-ui-bootstrap/blob/master/{{file}}'
+            },
+            api: {
+                src: ['src/**/*.js', '!src/**/*.spec.js'],
+                title: 'API Documentation'
+            }
+        },
+        connect: {
+            options: {
+                keepalive: true
+            },
+            server: {}
+        },
+        clean: ['<%= dist %>']
+    });
+    
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-angular-build');
+    grunt.loadNpmTasks('grunt-debug-task');
+    grunt.loadNpmTasks('grunt-ngdocs');
+    grunt.loadNpmTasks('grunt-contrib-connect');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    
+    grunt.registerTask('test', ['clean', 'build','ngdocs']);
+    grunt.registerTask('default', ['test', 'connect']);
+};
---
+++ src/form/form-field.js
@@ -0,0 +1,108 @@
+﻿(function () {
+    'use strict';
+    
+    /**
+    * @ngdoc overview
+    * @name sw.ui.bootstrap.form
+ 
+    * @description This module contains form controls
+    * - {@link sw.ui.bootstrap.form.directive:swFormField swFormField} directive    
+    */
+    angular.module('sw.ui.bootstrap.form', [])
+        .constant('formTemplatePath', 'template/form/');
+    
+    /**
+    * @ngdoc directive
+    * @name sw.ui.bootstrap.form.directive:swFormField
+    * @element ANY
+    * @restrict EA
+    * @require ngModel
+    * @scope    
+    * @function 
+    *
+    * @description
+    * Renders a form field. The following field types are supported: text, date
+    *
+    * @param {string} label The form field label
+    * @param {string} [type='text'] The field type. Acceptable values: 'text', 'date'
+    * @param {string} [placeholder=''] The field watermark
+    * @param {boolean} [inline=false] 
+    *   If true then the 'form-inline' css bootstrap class is used. 
+    *   If false then the 'form-group' css class is used    * 
+    * @param {ngModel} ngModel The {@link ng.directive:ngModel ngModel} directive attached to the associated input field
+    * @param {Object} options Additional field options 
+    * @param {string} [options.formatYear='yy'] Available for date field type.
+    * @param {Number} [options.startingDay=1] Available for date field type. 
+    * @param {datetime} [options.minDate] The minimum date allowed in the date-time picker
+    * @param {boolean} [options.required=false] True if the field is required, False otherwise
+    * 
+    * @example
+    <example module="sw.ui.bootstrap.form">
+     <file name="index.html">         
+        <sw-form-field label="Birth Date:" placeholder="Birth Date" type="date" data-ng-model="birthDate"></sw-form-field>       
+     </file>
+    </example>
+    */
+    angular.module('sw.ui.bootstrap.form', [])
+        .controller("FormController", ["$scope", 'formTemplatePath', function ($scope, formTemplatePath) {
+            function getTemplate(type) {
+                var name = (type) ? type : "text";
+                return (formTemplatePath + "field-" + name + ".html");
+            }
+            
+            function isDate(type) {
+                return type === "date";
+            }
+            
+            function updateFieldOptions(type, options) {
+                if (isDate(type)) {
+                    var defaultDateOptions = {
+                        formatYear: 'yy', 
+                        startingDay: 1,
+                        minDate: new Date()
+                    };
+                    $scope.dateOptions = $.extend(true, {}, defaultDateOptions, options);
+                } else {
+                    $scope.dateOptions = null;
+                }
+                $.extend(true, $scope.fieldOptions, options);
+            }
+            
+            $scope.fieldOptions = {
+                required: false
+            };
+            
+            $scope.$watch('type', function (type) {
+                $scope.template = getTemplate(type);
+                updateFieldOptions(type, $scope.options);
+            });
+            
+            $scope.$watch('options', function (newVal, oldVal) {
+                updateFieldOptions($scope.type, newVal);
+            }, true);
+            
+            $scope.open = function ($event) {
+                $event.preventDefault();
+                $event.stopPropagation();
+                
+                $scope.opened = true;
+            };
+        }])
+        .directive('swFormField', ['formTemplatePath', function (formTemplatePath) {
+            return {
+                restrict: 'EA',
+                require: '?ngModel',
+                scope: {
+                    label: '@',
+                    type: '@',
+                    placeholder: '@',
+                    title: '@',
+                    inline: '@',
+                    options: '=',
+                    ngModel: '='
+                },
+                controller: 'FormController',
+                templateUrl: formTemplatePath + 'form-field.html'
+            };
+        }]);
+})();
